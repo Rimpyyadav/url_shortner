c2629d215086bb2c885fb621086095b9bc4f92b8
Rimpy <yadavrimpy1624@gmail.com>
2024-01-27 12:54:52 +0530

    neew changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,8 @@
+const { getUser } = require("../service/auth")
+
 async function restrictToLoggedinUserOnly(req, res, next){
     const userUid = req.cookies.uid;
+    
 
     if(!userUid) return res.redirect("/login");
     const user = getUser(userUid);
